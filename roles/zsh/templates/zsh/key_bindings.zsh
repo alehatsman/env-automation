feaf4a97c287770a7a504eee785a31fe33a501c4
Aleh Atsman <aleh.atsman@gmail.com>
2020-02-28 15:21:20 +0100

    Zsh autocomplite on ctrl r

--- roles/zsh/templates/zsh/key_bindings.zsh
+++ roles/zsh/templates/zsh/key_bindings.zsh
@@ -1,7 +1,7 @@
 #---------------------------------------------
 # Bindings
 #---------------------------------------------
-bindkey '^ ' fzf-history-widget
+bindkey '^r' fzf-history-widget
 
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
