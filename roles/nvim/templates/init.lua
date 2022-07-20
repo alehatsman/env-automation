313ec8aaeb7d6937d03a829f0073ab4eb7da652e
Aleh Atsman <aleh.atsman@gmail.com>
2022-07-20 11:12:24 +0200

    sync

--- roles/nvim/templates/init.lua
+++ roles/nvim/templates/init.lua
@@ -311,10 +311,10 @@ vim.call('sign_define', 'DiagnosticSignHint', {text = "•", texthl = "Diagnosti
 
 -- lsp
 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
 vim.keymap.set('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>')
 vim.keymap.set('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n', '<c-]>', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<space>ds', '<cmd>lua vim.diagnostic.open_float()<CR>')
