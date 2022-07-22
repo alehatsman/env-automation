fc3be67c48bd4037e7dafb512c7947fed8d6f923
Aleh Atsman <aleh.atsman@gmail.com>
2022-07-22 09:07:06 +0200

    sync

--- roles/nvim/templates/init.lua
+++ roles/nvim/templates/init.lua
@@ -163,6 +163,7 @@ vim.g.vim_json_syntax_conceal = 0
 ---------------------------------------------
 -- Autocomplete
 ---------------------------------------------
+
 local cmp = require'cmp'
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
@@ -205,10 +206,40 @@ cmp.setup({
       name = 'nvim_lsp',
     },
     {
-      name = 'buffer',
-      max_item_count = 2,
+      name = 'path',
     },
-  })
+    { name = 'vsnip' },
+    {
+      name = 'buffer',
+      keyword_length = 5,
+      max_item_count = 10,
+    },
+  }),
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find "^_+"
+        local _, entry2_under = entry2.completion_item.label:find "^_+"
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
+
 })
 
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
