4597d2d14a2ffcf41c3436aff33c39e05dbda329
Aleh Atsman <aleh.atsman@gmail.com>
2022-09-24 21:28:43 +0200

    fix minimap

--- roles/nvim/templates/init.lua
+++ roles/nvim/templates/init.lua
@@ -526,5 +526,6 @@ vim.g.minimap_width = 10
 vim.g.minimap_auto_start = 1
 vim.g.minimap_auto_start_win_enter = 1
 vim.g.minimap_git_colors = 1
+vim.g.minimap_block_filetypes = { 'fugitive', 'nerdtree', 'tagbar', 'fzf', '' }
 
 vim.api.nvim_set_keymap('n', '<leader>mm', ':MinimapToggle<CR>', { noremap = true })
