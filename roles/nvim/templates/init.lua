01a1208cd3bb4338c39b0589f8615291e4393c05
Aleh Atsman <aleh.atsman@gmail.com>
2022-10-07 15:31:29 +0200

    prettier liquid

--- roles/nvim/templates/init.lua
+++ roles/nvim/templates/init.lua
@@ -381,7 +381,9 @@ null_ls.setup({
     null_ls.builtins.diagnostics.tsc,
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.formatting.eslint_d,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettier.with({
+      extra_filetypes = { "liquid" },
+    }),
     null_ls.builtins.formatting.jq,
   }
 })
